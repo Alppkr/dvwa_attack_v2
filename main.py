b80b109e528211bfd218c06a8b8819c304eb249b
alppkr <alp.pkr@gmail.com>
2023-06-25 09:46:13 -0400

    nice

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     'Login': 'Login'
     }
     #bruteForce("http://web-dvwa.example.com:30064/",'login.php',payload).startAttack()
-    commandInjection("http://web-dvwa.example.com:30064/",'login.php',payload).startAttack()
-    #IRA("http://web-dvwa.example.com:30064/",'login.php',payload).startAttack()
-    #RFI("http://web-dvwa.example.com:30064/",'login.php',payload).startAttack()
-    #sqlInjection("http://web-dvwa.example.com:30064/",'login.php',payload).startAttack()
-    #XSS("http://web-dvwa.example.com:30064/",'login.php',payload).startAttack()
+    #commandInjection("http://web-dvwa.example.com:30064/",'login.php',payload).startAttack()
+    IRA("http://web-dvwa.example.com:30064/",'login.php',payload).startAttack()
+    RFI("http://web-dvwa.example.com:30064/",'login.php',payload).startAttack()
+    sqlInjection("http://web-dvwa.example.com:30064/",'login.php',payload).startAttack()
+    XSS("http://web-dvwa.example.com:30064/",'login.php',payload).startAttack()
